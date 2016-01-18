c233a77fd7f0f9227a326e936a9ed218b94e57fc
Robert Manea <robert.manea@hotel.de>
2016-01-18 10:19:23 +0100

    Adapted URL regexes

--- notifications/urls.py
+++ notifications/urls.py
@@ -4,7 +4,7 @@ from .api import *
 
 urlpatterns = [
     url(r'messages/$', AllMessages.as_view()),
-    url(r'messages/(?P<platform>[a-zA-Z_-]+)/$', PlatformMessages.as_view()),
-    url(r'messages/(?P<platform>[a-zA-Z_-]+)/(?P<flavor>[a-zA-Z_-]+)/$', FlavorMessages.as_view()),
-    url(r'messages/(?P<platform>[a-zA-Z_-]+)/(?P<flavor>[a-zA-Z_-]+)/(?P<version>[0-9.]+)/$', VersionMessages.as_view()),
+    url(r'messages/(?P<platform>\w+)/$', PlatformMessages.as_view()),
+    url(r'messages/(?P<platform>\w+)/(?P<flavor>\w+)/$', FlavorMessages.as_view()),
+    url(r'messages/(?P<platform>\w+)/(?P<flavor>\w+)/(?P<version>\w+)/$', VersionMessages.as_view()),
 ]
